1829352e949f6dce997bd608b52ab7ef6946a4e8
ktorkian <kiarash.torkian@nike.com>
2019-10-19 14:48:11 -0700

    map location

--- app/src/main/java/com/restart/spotthatfire/ui/dashboard/ReportFragment.java
+++ app/src/main/java/com/restart/spotthatfire/ui/dashboard/ReportFragment.java
@@ -1,4 +1,4 @@
-package com.restart.spotthatfire.ui.notifications;
+package com.restart.spotthatfire.ui.dashboard;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -6,17 +6,17 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.restart.spotthatfire.R;
 
-public class NotificationsFragment extends Fragment {
+public class ReportFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_notifications, container, false);
+        return inflater.inflate(R.layout.fragment_dashboard, container, false);
     }
 }
