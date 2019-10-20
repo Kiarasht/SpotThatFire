09d439c175c356dcaa8aa14fe8034aaecf6d0eb7
ktorkian <kiarash.torkian@nike.com>
2019-10-20 01:18:40 -0700

    maps done

--- app/src/main/java/com/restart/spotthatfire/ui/home/MapsFragment.java
+++ app/src/main/java/com/restart/spotthatfire/ui/home/MapsFragment.java
@@ -1,30 +1,41 @@
 package com.restart.spotthatfire.ui.home;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Context;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.drawable.Drawable;
+import android.location.Criteria;
 import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
-import androidx.annotation.Nullable;
+import androidx.annotation.DrawableRes;
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProviders;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.restart.spotthatfire.R;
 
-public class MapsFragment extends Fragment implements OnMapReadyCallback, GoogleMap.OnMyLocationButtonClickListener, GoogleMap.OnMyLocationClickListener {
+public class MapsFragment extends Fragment implements OnMapReadyCallback {
 
     private GoogleMap mMap;
 
@@ -38,28 +49,70 @@ public class MapsFragment extends Fragment implements OnMapReadyCallback, Google
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         SupportMapFragment mapFragment = (SupportMapFragment) this.getChildFragmentManager().findFragmentById(R.id.map);
+        view.findViewById(R.id.main_fab).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ActivityCompat.requestPermissions(requireActivity(), new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, 999);
+                mMap.setMyLocationEnabled(true);
+                mMap.getUiSettings().setMyLocationButtonEnabled(true);
+
+                LocationManager locationManager = (LocationManager) requireActivity().getSystemService(Context.LOCATION_SERVICE);
+                @SuppressLint("MissingPermission") Location location = locationManager.getLastKnownLocation(locationManager.getBestProvider(new Criteria(), false));
+                if (location != null) {
+                    mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(location.getLatitude(), location.getLongitude()), 13));
+                    CameraPosition cameraPosition = new CameraPosition.Builder()
+                            .target(new LatLng(location.getLatitude(), location.getLongitude()))
+                            .zoom(8)
+                            .build();
+                    mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+                }
+            }
+        });
         mapFragment.getMapAsync(this);
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
+        Activity activity = requireActivity();
         mMap = googleMap;
-        // TODO: Before enabling the My Location layer, you must request
-        // location permission from the user. This sample does not include
-        // a request for location permission.
-
-
         ActivityCompat.requestPermissions(requireActivity(), new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, 999);
-    }
+        mMap.setMyLocationEnabled(true);
+        mMap.getUiSettings().setMyLocationButtonEnabled(true);
 
-    @Override
-    public boolean onMyLocationButtonClick() {
-        return false;
-    }
+        LocationManager locationManager = (LocationManager) requireActivity().getSystemService(Context.LOCATION_SERVICE);
+        @SuppressLint("MissingPermission") Location location = locationManager.getLastKnownLocation(locationManager.getBestProvider(new Criteria(), false));
+        if (location != null) {
+            LatLng newPosition = new LatLng(location.getLatitude() + 1, location.getLongitude() + 1);
 
-    @Override
-    public void onMyLocationClick(@NonNull Location location) {
+            mMap.addMarker(new MarkerOptions()
+                    .icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_fire))
+                    .anchor(0.5f, 0.5f) // Anchors the marker on the bottom left
+                    .position(newPosition));
 
+            double magnitude = 5;
+            int stroke, fill;
+
+            /* Circles color depends on the magnitude of an earth quake. */
+            //noinspection ConstantConditions
+            if (magnitude > 8) {
+                stroke = ContextCompat.getColor(activity, R.color.strokeCircleDanger);
+                fill = ContextCompat.getColor(activity, R.color.fillCircleDanger);
+            } else if (magnitude > 4) {
+                stroke = ContextCompat.getColor(activity, R.color.strokeCircleWarning);
+                fill = ContextCompat.getColor(activity, R.color.fillCircleWarning);
+            } else {
+                stroke = ContextCompat.getColor(activity, R.color.strokeCircleSafe);
+                fill = ContextCompat.getColor(activity, R.color.fillCircleSafe);
+            }
+
+            /* Circles size depends slightly on the magnitude of an earth quake. */
+            mMap.addCircle(new CircleOptions()
+                    .center(newPosition)
+                    .radius(1000 * 5)
+                    .strokeColor(stroke)
+                    .fillColor(fill));
+
+        }
     }
 
     @Override
@@ -67,8 +120,6 @@ public class MapsFragment extends Fragment implements OnMapReadyCallback, Google
         if (requestCode == 999) {
             if (permissions.length == 1 && permissions[0].equals(Manifest.permission.ACCESS_FINE_LOCATION) && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 mMap.setMyLocationEnabled(true);
-                mMap.setOnMyLocationButtonClickListener(this);
-                mMap.setOnMyLocationClickListener(this);
                 mMap.getUiSettings().setMyLocationButtonEnabled(true);
             }
         }
