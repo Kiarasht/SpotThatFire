2c44943d0707a56f7b574f7eaf3eb89ccc6a7bb8
Rohan Elukurthy <rohan.elukurthy@volteo.com>
2019-10-19 18:45:42 -0700

    feat(app): adding s3 image and metadata to dynamo

--- aws/lambda/index.js
+++ aws/lambda/index.js
@@ -4,6 +4,8 @@ const EXIF = require('exifreader')
 
 //*/ get reference to S3 client 
 var s3 = new AWS.S3();
+var dynamodb = new AWS.DynamoDB({apiVersion: '2012-08-10'});
+
 exports.handler = (event, context, callback) => {
      let encodedImageString = JSON.parse(event.body).image;
      let image_id = UUID();
@@ -16,7 +18,7 @@ exports.handler = (event, context, callback) => {
        "ContentEncoding": 'base64',
        "ContentType": 'image/jpg'
     };
-    console.log("ijsflnvsk")
+
     s3.upload(params, function(err, data){
        if(err) {
            callback(err, null);
@@ -36,4 +38,22 @@ exports.handler = (event, context, callback) => {
 
     console.log(image_metadata);
     
+
+    var params = {
+        Item: {
+         "id": { S: image_id},
+         "latitude": { S: ""+image_metadata.GPSLatitude.description}, 
+         "longitude": { S: ""+image_metadata.GPSLatitude.description * -1},
+         "s3Path": {S: "https://fire-app-data.s3-us-west-2.amazonaws.com/images/"+ image_id +".jpg"},
+         "timestamp": {S: image_metadata.DateTimeOriginal.description}
+        }, 
+        ReturnConsumedCapacity: "TOTAL", 
+        TableName: "fire_images"
+       };
+       dynamodb.putItem(params, function(err, data) {
+         if (err) console.log(err, err.stack); // an error occurred
+         else     console.log(data);           // successful response
+       });
+
+
 };
